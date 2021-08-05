0a2d618c98d820872be417fef7599db7c514ddd4
Kolya <amigo.bro.657@gmail.com>
2021-08-05 22:02:20 +0300

    Snake and area

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ game_scrin = pg.display.set_mode(
 )
 pg.display.set_caption("snake")
 game_scrin.fill(Color.BLUE)
-work = True
-while work:
-    keys = pg.key.get_pressed()
-    work = not keys[pg.K_ESCAPE]
-    timer.tick(fps)
-    events = pg.event.get()
-    for ev in events:
-        print(ev)
-        if ev.type == pg.WINDOWCLOSE:
-            work = False
+# work = True
+# while work:
+#     keys = pg.key.get_pressed()
+#     work = not keys[pg.K_ESCAPE]
+#     timer.tick(fps)
+#     events = pg.event.get()
+#     for ev in events:
+#         print(ev)
+#         if ev.type == pg.WINDOWCLOSE:
+#             work = False
 
-pg.quit()
-# __pycache__/
+# pg.quit()
+# # __pycache__/
---
+++ play/game.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+
+class Game(pg.Surface):
+    def __init__(self):
+        ...
+    
+    def run(self):
+        ...
+
+    def get_direction(self):
+        ...
+
+    def update(self):
+        ...
+
+    
+    
